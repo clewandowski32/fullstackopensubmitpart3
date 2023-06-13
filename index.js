6dd8ddf466ac2db17e62bac0d9b2ea78d184ae0d
clewandowski32 <lewandoc@umich.edu>
2023-06-13 19:52:41 -0400

    finished up to 3.6

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    },
+    {
+        "id": 5,
+      "name": "Christian Lewandowski", 
+      "number": "330-607-8899"
+    }
+]
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p>
+                  ${Date()}`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if(person){
+        response.json(person)
+    }
+
+    else{
+        response.status(404).end()
+    }
+})
+
+const generateId = () => {
+    return Math.random() * 1000
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if(!body.name || !body.number){
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+    if(persons.find(p => p.name === body.name)){
+        return response.status(400).json({
+            error: 'person already exists'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${3001}`)
+})
